29aea7b7ebc4d02e2dac76aa096b439e6e48cc09
rospetbaty <petros@sourced.tech>
2016-12-18 17:48:39 +0100

    Finalized Styling

--- src/NewSearch.js
+++ src/NewSearch.js
@@ -1,8 +1,8 @@
 //GET user function
-
 function getUserName() {
-    var nameField = document.getElementById('nameField').value;
-    var gitUser = ("https://api.github.com/users/"+nameField);
+    var nameField = document.getElementById('nameField').value
+    var gitUser = ("https://api.github.com/users/"+nameField)
+    clear()
     fetch(gitUser)
     .then(resp => {
       if (resp.status >= 300 || resp.status < 200) {
@@ -12,14 +12,20 @@ function getUserName() {
     })
     .then(data => {
       let view = document.getElementById("view")
-      view.innerHTML = `<div>
-        <img width="150" height="150" align="left" src="${data['avatar_url']}" />
-        <p style="text-align:right"><font size="2" color="grey">@${data['login']}
-        </font><br/><em><b>${data['name']}</b></em><br/>
-        ${data['bio']}</p>
+      let bio = data['bio'] ? data['bio'] : 'This user has no description'
+      view.innerHTML = `
+        <div class="userinfo">
+          <div class="useravatar">
+            <img width="150" height="150" align="left" src="${data['avatar_url']}" />
+          </div>
+          <div class="userdescription">
+            <span class="userhandle">@${data['login']}</span>
+            <span class="name">${data['name']}</span>
+            <span class="userbio">${bio}</span>
+          </div>
         </div>`
         // calling repoList
-        repoList();
+        repoList()
     })
     .catch(_ => {
       let view = document.getElementById("view")
@@ -28,10 +34,17 @@ function getUserName() {
 }
 
 
+function clear() {
+  let view = document.getElementById("view")
+  let repos = document.getElementById("repos")
+  view.innerHTML = ""
+  repos.innerHTML = ""
+}
+
 //function for displaying repo list
 function repoList(){
   var nameField = document.getElementById('nameField').value;
-  var gitUser = ("https://api.github.com/users/"+nameField+"/repos");
+  var gitUser = ("https://api.github.com/users/"+nameField+"/repos")
   fetch(gitUser)
   .then(resp => {
     if (resp.status >= 300 || resp.status < 200) {
@@ -40,22 +53,24 @@ function repoList(){
     return resp.json()
   })
   .then(data => {
-  const container = document.getElementById("repos");
-  if (data.length > 0) {
-    const repos = data.map(repo => `<li class="repolist">
-      <a href="${repo.html_url}">${repo.name}</a>
-      <span style="padding-left:10px;text-align:right;" class="stars">
-      <img src= ./images/Octicons-star.png height="15" width="15">
-      ${repo.stargazers_count}</span>
-      <span class="forks"><img src= ./images/fork.svg height="15" width="15">
-      ${repo.forks_count}</span>
-    </li>`).join('');
-    container.innerHTML = `<br></br><h2 class="repotitle">Repositories</h2>
-    <ul>${repos}</ul>`;
-  } else {
-    container.innerHTML = "<p>This user has no repos</p>";
-  }
-});
+  let container = document.getElementById("repos")
+    if (data.length > 0) {
+      let repos = data.map(repo => `<li class="repo">
+        <a href="${repo.html_url}">${repo.name}</a>
+        <div class="social">
+          <span class="stars">
+            <img src= ./images/Octicons-star.png height="15" width="15">${repo.stargazers_count}
+          </span>
+          <span class="forks">
+            <img src= ./images/fork.svg height="15" width="15"> ${repo.forks_count}
+          </span>
+        </div>
+      </li>`).join('');
+      container.innerHTML = `<h2 class="repotitle">Repositories</h2> <ul class="reposlist">${repos}</ul>`;
+    } else {
+      container.innerHTML = `<h2 class="repotitle">Repositories</h2> <p>This user has no repos</p>`;
+    }
+  });
   };
 
 //start search by pressing Return instead of clicking button
